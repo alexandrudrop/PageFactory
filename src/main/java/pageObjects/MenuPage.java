f706ee60bd6dbb3022d3120db1f65243a6b20ab9
alexandru.drop <alexandru.drop@gmail.com>
2024-06-28 18:08:33 +0300

    curs 33

---
+++ src/main/java/pageObjects/MenuPage.java
@@ -0,0 +1,24 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import selenium.utils.SeleniumWrappers;
+
+public class MenuPage extends SeleniumWrappers{
+	
+	public MenuPage(WebDriver driver) {
+		super(driver);
+		PageFactory.initElements(driver, this);
+	}
+	
+	//public By myAccountLink = By.linkText("My account");
+	
+	@FindBy(linkText = "My account")
+	public WebElement myAccountLink;
+	
+	
+
+}
